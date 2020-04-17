9c7353128dc62e9d9a7a72d454184e79f758fb27
Guillaume Lucazeau <glucazeau@advanced-schema.com>
2020-04-17 13:18:47 +0200

    Map roles columns

--- src/main/java/fr/croixrouge/paris/aalf/role/Role.java
+++ src/main/java/fr/croixrouge/paris/aalf/role/Role.java
@@ -1,18 +1,25 @@
 package fr.croixrouge.paris.aalf.role;
 
 import fr.croixrouge.paris.aalf.user.UserEntity;
+import lombok.Data;
 
 import javax.persistence.*;
 import java.util.Set;
 
 @Entity
+@Table(name = "roles")
+@Data
 public class Role {
   
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
- 
-    private String name;
+
+    @Column(name = "label")
+    private String label;
+
+    @Column(name = "role")
+    private String value;
 
     @OneToMany
     private Set<UserEntity> users;
