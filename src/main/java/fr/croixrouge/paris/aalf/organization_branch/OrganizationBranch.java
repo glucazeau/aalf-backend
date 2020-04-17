42e642339e88a5dd4992947b2744147199642a56
Guillaume Lucazeau <glucazeau@advanced-schema.com>
2020-04-17 12:55:02 +0200

    Add branch entity

---
+++ src/main/java/fr/croixrouge/paris/aalf/organization_branch/OrganizationBranch.java
@@ -0,0 +1,38 @@
+package fr.croixrouge.paris.aalf.organization_branch;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+import javax.persistence.*;
+
+import static lombok.AccessLevel.PRIVATE;
+
+@Entity
+@Table(name = "organization_branch")
+@Data
+@FieldDefaults(level = PRIVATE)
+@NoArgsConstructor
+public class OrganizationBranch {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column
+    String name;
+
+    @Column
+    String address;
+
+    @Column(name = "president_name")
+    String presidentName;
+
+    @Column(name = "phone_number")
+    String phoneNumber;
+
+    @Column(name = "fax_number")
+    String faxNumber;
+
+    @Column(name = "email_address")
+    String emailAddress;
+}
