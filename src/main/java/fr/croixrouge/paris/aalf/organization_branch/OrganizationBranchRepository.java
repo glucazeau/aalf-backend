fb591ad20ada55af3ef513a860c52aab5a6b7666
Guillaume Lucazeau <glucazeau@advanced-schema.com>
2020-04-18 15:51:53 +0200

    Fix org branch repository

--- src/main/java/fr/croixrouge/paris/aalf/organization_branch/OrganizationBranchRepository.java
+++ src/main/java/fr/croixrouge/paris/aalf/organization_branch/OrganizationBranchRepository.java
@@ -1,10 +1,8 @@
 package fr.croixrouge.paris.aalf.organization_branch;
 
-import fr.croixrouge.paris.aalf.user.UserEntity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(collectionResourceRel = "users", path = "users")
-public interface OrganizationBranchRepository extends CrudRepository<UserEntity, Integer> {
-    UserEntity findByEmailAddress(final String emailAddress);
+@RepositoryRestResource(collectionResourceRel = "organizationBranches", path = "organizationBranches")
+public interface OrganizationBranchRepository extends CrudRepository<OrganizationBranch, Integer> {
 }
