cae574c5fe18f081411b19b238f0837ca21ac9ae
Guillaume Lucazeau <glucazeau@advanced-schema.com>
2020-04-19 16:16:45 +0200

    Drop unused tables

--- src/main/resources/db/migration/V3__migrate_old_app.sql
+++ src/main/resources/db/migration/V3__migrate_old_app.sql
@@ -1,3 +1,13 @@
+DROP TABLE a_repondu;
+DROP TABLE acl_entries;
+DROP TABLE acl_object_identities;
+DROP TABLE acl_object_identity_ancestors;
+DROP TABLE acl_security_identities;
+DROP TABLE acl_classes;
+DROP TABLE acl_object_identities;
+DROP TABLE adresse_appartient_a;
+DROP TABLE adresses;
+
 ALTER TABLE teacher DROP salt;
 ALTER TABLE teacher DROP username;
 ALTER TABLE teacher DROP username_canonical;
